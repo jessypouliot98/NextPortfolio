e10c7d17eb5e38c27944510a015292582cc9d712
Jessy Pouliot <jessypouliot98@gmail.com>
2022-10-20 22:56:15 -0400

    fix db

--- prisma/migrations/20221021025606_initial_db/migration.sql
+++ prisma/migrations/20221021025606_initial_db/migration.sql
@@ -9,3 +9,15 @@ CREATE TABLE "Comment" (
 
     CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "Blog" (
+    "id" SERIAL NOT NULL,
+    "contentfulEntryId" VARCHAR(64) NOT NULL,
+    "views" INTEGER NOT NULL,
+
+    CONSTRAINT "Blog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Blog_contentfulEntryId_key" ON "Blog"("contentfulEntryId");
